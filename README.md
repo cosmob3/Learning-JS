77e64a0cbc5bad488003e337876051b61d4eb8b5
cosmob3 <bryanlvelasco@hotmail.com>
2022-08-14 00:44:26 -0600

    working on objects

---
+++ README.md
@@ -0,0 +1,5 @@
+## AUGUST 14, 2022
+
+- Should probably keep notes of my progress. Something to look back on for help or to be able to see my progress.
+- Working on JavaScript Objects.
+- Now I understand a lot better the basics of objects.
---
+++ object.js
@@ -0,0 +1,19 @@
+let pen = {
+  type: "ballpoint",
+  color: "black",
+  cost: 10,
+};
+
+// To access an object
+// objectName.propertyName
+console.log(pen.cost);
+// Outcome: 10
+
+let emily = {
+  age: 16,
+  school: "DPS",
+  class: "10th std",
+  subjects: ["physics", "chemistry", "biology", "maths"],
+};
+console.log(emily.subjects[1]);
+//Outcome: "chemistry"
