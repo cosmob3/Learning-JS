a5aa5ae5b106d0fd39e75ad3a20d94c471bf1038
cosmob3 <bryanlvelasco@hotmail.com>
2022-08-16 01:49:54 -0600

    Learnig what a switch in JS is

---
+++ switch.js
@@ -0,0 +1,18 @@
+let games = "catch";
+switch (games) {
+  case "throwball":
+    console.log("I hate throwball");
+    break;
+  case "basketball":
+    console.log("basketball is hard");
+    break;
+  case "cricket":
+    console.log("I'm a huge cricket fan");
+    break;
+  case "football":
+    console.log("I love football");
+    break;
+  default:
+    console.log("I like other games");
+    break;
+}
