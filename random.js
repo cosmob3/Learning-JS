840aeb6a21318c0b171fe112079d5f86a79ff14e
cosmob3 <bryanlvelasco@hotmail.com>
2022-09-02 00:11:32 -0600

    learned alert()

---
+++ random.js
@@ -0,0 +1,11 @@
+// alert("Hi, my name is BRYAN");
+// Output: This will give you a pop up that says "Hi, my name is BRYAN" every time the page is reloaded
+
+const consLog = "If everything went as expected";
+console.log(consLog);
+
+const consWarn = "If something went wrong";
+console.warn(consWarn);
+
+const consError = "Not working/Error";
+console.error(consError);
